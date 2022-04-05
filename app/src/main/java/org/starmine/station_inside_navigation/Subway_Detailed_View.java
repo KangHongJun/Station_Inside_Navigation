aac52300ae43d9bead763d1db570a4ec0cbaf3b6
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2022-04-05 18:17:18 +0900

    use kakao_map

--- app/src/main/java/org/starmine/station_inside_navigation/Subway_Detailed_View.java
+++ app/src/main/java/org/starmine/station_inside_navigation/Subway_Detailed_View.java
@@ -1,9 +1,15 @@
 package org.starmine.station_inside_navigation;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,10 +17,22 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Subway_Detailed_View extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subway_detailed_view);
+
+        //카카오 map
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.Kakao_map);
+        mapViewContainer.addView(mapView);
+
+        getDebugHashKey();
 
         //툴바 세팅
         Toolbar toolbar = findViewById(R.id.Detail_Toolbar);
@@ -30,7 +48,7 @@ public class Subway_Detailed_View extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.detail_menu,menu);
+        menuInflater.inflate(R.menu.detail_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -39,13 +57,35 @@ public class Subway_Detailed_View extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int curId = item.getItemId();
-        switch (curId){
+        switch (curId) {
             case R.id.menu_refresh:
-                Toast.makeText(this,"설정메뉴",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "설정메뉴", Toast.LENGTH_LONG).show();
                 break;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void getDebugHashKey() {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            if (packageInfo == null)
+                Log.e("KeyHash", "KeyHash:null");
+            else {
+                for (Signature signature : packageInfo.signatures) {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
